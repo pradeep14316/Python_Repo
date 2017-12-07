da19a0f8c475716c0c81def54f3f39d947ce4719
pradeep14316 <pradeep.vangala@yahoo.com>
2017-12-07 16:11:20 +0000

    Updated self_dict.py file

--- python_basics/student_dict.py
+++ python_basics/student_dict.py
@@ -1,16 +1,28 @@
-students={}
+students=[]
 
-def add_students():
-    count = 0
+def add_students(student_name='',student_id=''):
     while True:
-        student_names=input("Enter the student name to enroll:")
-        count+=1
-        student_id=count
-        students[list]=student_id
-        prompt=input("if you want to continue (Y/N):")
-        if prompt=='y' or prompt=='Y':
+
+        # ask the user to enter the values needed
+        student_name = input("Enter the student name to enroll:")
+        student_id = int(input("Enter the student Id:"))
+
+        #Appending  values to students list
+        students_list = {"Name": student_name, "Student_ID": student_id}
+        students.append(students_list)
+
+        # prompt user to continue or not
+        prompt = input("if you want to continue (Y/N):")
+
+
+        if prompt == 'y' or prompt == 'Y':
             continue
-        elif prompt=='n' or 'N':
+        elif prompt == 'n' or 'N':
             break
-    print(students.items())
+        else:
+            print("Completed enrollment. Here is the list of Students Enrolled")
+
+    print(students)
+
+
 add_students()
