6e123dc485375bf2b37135df2752e860fc6074df
pradeep14316 <pradeep.vangala@yahoo.com>
2018-01-14 20:52:22 +0000

    deleted files from python basics

--- Python_exercise/python_basics/generators.py
+++
@@ -1,9 +0,0 @@
-str='pradeep'
-def test():
-    for i in testgen(str):
-        print(i)
-
-
-def testgen(c):
-    for x in
-test()
--- python_basics/user.py
+++ python_basics/user.py
@@ -13,25 +13,15 @@ class User:
         self.last_name = full_name[-1]
 
     def age(self):
-        try:
-            today = datetime.date(2017, 12, 8)
-            yyyy= self.dob[0:4]
-            mm = self.dob[4:6]
-            dd = self.dob[6:8]
-            user_dob = datetime.date(yyyy, mm, dd)
-            age_in_days = (today - user_dob).days
-            age_in_years = (age_in_days / 365)
-            return int(age_in_years)
-        except ImportError:
-            print("There is something not yet found")
-        except IOError:
-            print("check your input")
-        except TypeError as e:
-            print("Check the error",e)
-        else:
-            print("write a different code")
-        finally:
-            print("This is finaly executed")
+        today = datetime.date(2017, 12, 8)
+        yyyy= self.dob[0:4]
+        mm = self.dob[4:6]
+        dd = self.dob[6:8]
+        user_dob = datetime.date(yyyy, mm, dd)
+        age_in_days = (today - user_dob).days
+        age_in_years = (age_in_days / 365)
+        return int(age_in_years)
+
 
 user = User('Mathews H Gilbert', 19891227)
 print('First_Name:', user.first_name)
