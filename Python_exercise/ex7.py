6f8f2006c8b88d316762e8b732f8abc5a7ecbc33
pradeep14316 <pradeep.vangala@yahoo.com>
2018-01-14 20:37:39 +0000

    clean the repository

--- Python_exercise/ex7.py
+++
@@ -1,8 +0,0 @@
-#find the sum of digits in the number 100!
-def factor(num):
-    n = 1
-    for i in range(2, num+1):
-        n = n * i
-        addition=sum(int(i) for i in str(n))
-    return "sum of ", n, "is ", addition
-print(factor(100))
--- Python_exercise/ginga.py
+++
@@ -1,6 +0,0 @@
-import re
-
-text="Bounce Hi welcome to Bounce the World yo gonna rock it here "
-regex=
-result=re.search('^[BW].+\w ', text)
-print(result.group())
